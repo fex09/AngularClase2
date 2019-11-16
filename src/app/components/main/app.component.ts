ae3788d3d46578815369a37c036d59b3911bf282
Solís González Fenix <FeSoli@ice.go.cr>
2019-11-15 19:45:14 -0600

    componente nuevo

--- src/app/components/main/app.component.ts
+++ src/app/components/main/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+// Models
+import { Product } from 'src/app/models/product';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'StoreAngular';
+
+  public products: Product[];
+
+  constructor(){
+    this.products = [];
+    let p1 = new Product();
+    p1.Id = 0;
+    p1.Name = 'HP Envy';
+    p1.Description = 'Laptop';
+    p1.Image = 'https://dummyimage.com/100X100/000/fff';
+    p1.Quantity = 3;
+    p1.Price = 1000;  
+
+    let p2 = new Product();
+    p2.Id = 0;
+    p2.Name = 'Dell Inspiron';
+    p2.Description = 'Laptop';
+    p2.Image = 'https://dummyimage.com/100X100/000/fff';
+    p2.Quantity = 5;
+    p2.Price = 700;  
+
+    this.products.push(p1);
+    this.products.push(p2);
+  }
 }
