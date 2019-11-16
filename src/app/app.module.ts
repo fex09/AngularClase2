b6c8ba09d5d05fd1444535d290f0576fd1cac009
Solís González Fenix <FeSoli@ice.go.cr>
2019-11-15 19:00:30 -0600

    Creación de sitio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/main/app.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/models/product.ts
@@ -0,0 +1,7 @@
+export class Product{
+    public Name: string;
+    public Description: string;
+    public Image: string;
+    public Price: number;
+    public Quantity: number;
+}
